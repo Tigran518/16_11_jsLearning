fda1ea24d560546572ff090180d6891e05ea6d4a
Tigran518 <tigrangalstyan176@gmail.com>
2024-11-19 22:00:22 +0400

    learn for let const

---
+++ index.js
@@ -0,0 +1,52 @@
+// var num1 = +prompt("Enter the first number:");
+// var num2 = +prompt("Enter the second number:");
+// var operation = prompt("Choose the operation (+, -, *, /):");
+
+// switch (operation) {
+//     case '+': console.log(num1 + num2); break;
+//     case '-': console.log(num1 - num2); break;
+//     case '*': console.log(num1 * num2); break;
+//     case '/': 
+//         if (num2 != 0) {
+            
+//         }
+//     console.log(num1 / num2); break;
+//     default: console.log('invalid input');
+// }
+
+// console.log(x);
+// var x = 10;
+
+// if(true){
+//     let x = 10//var
+//     //let x =30;
+// }
+// console.log(x);
+
+// const x;
+// x=10;
+
+// var x = 10;
+// console.log(x)
+
+// let count = 0;
+// count = count + 1;
+// count = count + 1;
+// count = count + 1;
+// count += 1;
+// count++;
+// console.log(count++);
+// console.log(++count);
+
+let i = 20;
+for (let i =0; i < 10; i++) {    
+    console.log(i)
+}
+
+console.log(i,'in global')
+
+var x = 20;
+if (true) {
+    var x = 10;
+    console.log(x)
+}
